e189338bb9ab75a3f68ab61a3b124dc9a54a4953
AndrewTek <tekunov.andrei@gmail.com>
2025-03-09 20:25:29 +0300

    Исправил тест

--- src/test/java/GitJunitTest.java
+++ src/test/java/GitJunitTest.java
@@ -21,8 +21,17 @@ public class GitJunitTest {
         open("https://github.com/selenide/selenide");
         $("#wiki-tab").click();
         $("#wiki-body").$(byText("Soft assertions")).click();
-        $(".markdown-body").shouldHave(text("ExtendWith"));
-
+        $(".markdown-body").shouldHave(text("@ExtendWith({SoftAssertsExtension.class})\n" +
+                "class Tests {\n" +
+                "  @Test\n" +
+                "void test() {\n" +
+                "Configuration.assertionMode = SOFT;\n" +
+                "open(\"page.html\");\n" +
+                "\n" +
+                "$(\"#first\").should(visible).click();\n" +
+                "$(\"#second\").should(visible).click();\n" +
+                "}\n" +
+                "}"));
     }
 
 }
